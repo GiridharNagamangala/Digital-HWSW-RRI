126745cdc1b2b5f7ebbe917de6575078fac2b6bf
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-06-25 16:52:45 +0530

    Updated MATLAB plot file
    Added ADC parameters calculation

--- Software and Simulation/ADC_FPGA_OutputRead.m
+++ Software and Simulation/ADC_FPGA_OutputRead.m
@@ -2,14 +2,25 @@ fid = fopen('ADC_data.txt', 'r'); % To add filepath
 count = inf;
 format = int16; % What's the .txt file format?
 
+% User input for splice filter
+bandcut = input('Enter filtering rate for noise (between zero and one): ', "s");
+
 % Time and Spectrum data
 ADC_data = fread(fid, format);
 spectral_mag = abs(fft(ADC_data));
 spectral_pow = spectral_mag ^ 2;
 
 % ADC Parameters
-signal_pow = max(spectral_pow); % Power level of signal
-% noise_pow % Power level of noise
+[signal_pow, h_idx] = max(spectral_pow); % Power level of signal
+onlyNoise = spectral_pow(spectral_pow < bandcut);
+noise_pow = max(onlyNoise); % Power level of noise
+disto_pow = spectral_pow(h_idx * 2) + spectral_pow(h_idx * 3) + spectral_pow(h_idx * 4) + spectral_pow(h_idx * 5) + spectral_pow(h_idx * 6);
+
+SNR = 10 * log10(signal_pow / noise_pow); % SNR
+SNR_ideal = (6.02 * 14) + 1.76; % Ideal SNR
+THD = 10 * log10(signal_pow / disto_pow); % THD
+SINAD = 10 * log10(signal_pow / (noise_pow + disto_pow)); % SINAD
+ENOB = (SINAD - 1.76) / 6.02; % ENOB
 
 % Plot graphs
 plot(ADC_data);
